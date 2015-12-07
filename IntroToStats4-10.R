abf73a65fc6e4741193265fc1b404a7ec759a2b0
Lingyun Xiao <bowlxly@gmail.com>
2015-12-07 14:36:49 -0500

    Intro to Stats 3rd Commit

--- IntroToStats4-10.R
+++ IntroToStats4-10.R
@@ -12,10 +12,22 @@ summary(Weekly)
 
 pairs(Weekly)
 cor(Weekly[,-ncol(Weekly)])
-
 #Obvious correlation between volume and year
 
 glm.fit=glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume, data=Weekly ,family=binomial)
 summary(glm.fit)
-#Lag2 has obvious importance / effect
+#Lag2 is statistically significant
 
+coef(glm.fit)
+summary(glm.fit)$coef
+#Coefficients of the model
+
+glm.probs=predict(glm.fit,type="response") 
+contrasts(Weekly$Direction)
+#Convert Probabilities to Up and Down
+
+glm.pred=rep("Down","Up",nrow(Weekly))
+glm.pred[glm.probs >.5]="Up"
+table(glm.pred,Direction)
+mean(glm.pred==Direction )
+#Calculate the probability of making right prediction
