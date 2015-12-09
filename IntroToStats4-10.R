a31335742daa544db03273398169441a4c8a4435
Lingyun Xiao <bowlxly@gmail.com>
2015-12-09 07:10:09 -0500

    Intro to Stats 4-10 (Stock Market) Final Commit

--- IntroToStats4-10.R
+++ IntroToStats4-10.R
@@ -29,5 +29,42 @@ contrasts(Weekly$Direction)
 glm.pred=rep("Down","Up",nrow(Weekly))
 glm.pred[glm.probs >.5]="Up"
 table(glm.pred,Direction)
-mean(glm.pred==Direction )
-#Calculate the probability of making right prediction
+mean(glm.pred==Direction)
+#Calculate confusion matrix and the probability of making right prediction
+
+train =( Year <= 2008)
+Weekly.testing= Weekly [! train ,]
+glm.fit=glm(Direction~Lag2,data=Weekly ,family=binomial, subset=train)
+summary(glm.fit)
+glm.probs=predict(glm.fit,Weekly.testing,type="response")
+glm.pred=rep("Down",nrow(Weekly.testing))
+glm.pred[glm.probs >.5]="Up"
+table(glm.pred,Weekly.testing$Direction)
+mean(glm.pred==Weekly.testing$Direction)
+#Logistic Model, Using Lag2 as the only predictor,Prob=0.625
+
+library(MASS)
+lda.fit = lda(Direction ~ Lag2, data=Weekly, subset=train)
+lda.fit
+lda.class=predict(lda.fit,Weekly.testing)$class
+table(lda.class,Weekly.testing$Direction)
+mean(lda.class==Weekly.testing$Direction)
+#Linear Discriminant Analysis,Prob=0.625
+
+qda.fit = qda(Direction ~ Lag2, data=Weekly.testing, subset=train)
+qda.fit
+qda.class=predict(qda.fit,Weekly.testing)$class
+table(qda.class,Weekly.testing$Direction)
+mean(qda.class==Weekly.testing$Direction)
+#Quadratic Discriminant Analysis,Prob=0.63
+
+library(class)
+train.X=Weekly[train,"Lag2",drop=F]
+test.X=Weekly[!train,"Lag2",drop=F]
+train.Direction=Weekly[train,"Direction",drop=T]
+test.Direction=Weekly[!train,"Direction",drop=T]
+set.seed(1)
+knn.pred=knn(train.X,test.X,train.Direction,k=5)
+table(knn.pred,test.Direction)
+mean(knn.pred==test.Direction)
+#K-Nearest Neighbors,Prob=0.5 when k=1, Prob=0.62 when k=4
